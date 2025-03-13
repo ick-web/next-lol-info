cb4d54f3bc28744784df2318de3d147265ccc2b3
ick-web <yih000098@gmail.com>
2025-03-13 20:09:49 +0900

    fix: css 수정

--- src/components/champions/ChampionCard.tsx
+++ src/components/champions/ChampionCard.tsx
@@ -8,8 +8,8 @@ export default function ChampionCard({ champion }: ChampionCardProps) {
   return (
     <li className="container">
       <Link href={`/champions/${champion.id}`}>
-        <div className="relative flex flex-col items-center border-2 rounded-md shadow-md bg-gray-200 p-4 hover:scale-105 transition-transform duration-300">
-          <div className="w-32 h-32 rounded-full overflow-hidden border-2 border-black">
+        <div className="relative flex flex-col items-center rounded-md border-2 bg-gray-200 p-4 shadow-md transition-transform duration-300 hover:scale-105">
+          <div className="h-32 w-32 overflow-hidden rounded-full border-2 border-black">
             <Image
               src={`https://ddragon.leagueoflegends.com/cdn/${champion.version}/img/champion/${champion.image.full}`}
               priority={true}
@@ -18,8 +18,8 @@ export default function ChampionCard({ champion }: ChampionCardProps) {
               alt={champion.id}
             />
           </div>
-          <div className="flex flex-col mt-2 text-center">
-            <h3 className="font-semibold">{champion.name}</h3>
+          <div className="mt-2 flex flex-col text-center">
+            <h3 className="font-semibold text-red-500">{champion.name}</h3>
             <p className="text-ws text-gray-500">{champion.title}</p>
           </div>
         </div>
