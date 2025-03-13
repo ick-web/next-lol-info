197e0a4e2fb07405daa73ad2ec624d8ccee1196f
ick-web <yih000098@gmail.com>
2025-03-13 20:08:36 +0900

    feat: metadata 추가

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -7,6 +7,19 @@ type Props = {
     }
 }
 
+export const generateMetadata = async ({ params }: Props) => {
+    const { id } = params
+    const data = await fetchChampionDetail(id)
+    const champion = data.data
+    const version = data.version
+  
+    return {
+      title: `${champion.name} - ${champion.title}`,
+      description: champion.blurb,
+      image: `https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champion.image.full}`,
+    }
+  }
+
 export default async function ChampionDetailPage({ params }: Props){
     const { id } = params
     const championDetail = await fetchChampionDetail(id)
