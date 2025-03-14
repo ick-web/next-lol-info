d068f345c915b7fd313509517427fd03ba59ebbe
ick-web <yih000098@gmail.com>
2025-03-14 20:53:29 +0900

    🎨 design: 스타일링

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,21 +1,31 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   return (
-    <header className="flex flex-row justify-around p-4 bg-gray-800">
+    <header className="flex flex-row justify-around bg-gray-800 p-4">
       <div>
         <Link href="/" className="text-white">
           홈
         </Link>
       </div>
-      <Link href="/champions" className="text-white">
+      <Link
+        href="/champions"
+        className="flex items-center justify-center text-white"
+      >
         챔피언 목록
       </Link>
-      <Link href="/items" className="text-white">
+      <Link
+        href="/items"
+        className="flex items-center justify-center text-white"
+      >
         아이템 목록
       </Link>
-      <Link href="/rotation" className="text-white">
+      <Link
+        href="/rotation"
+        className="flex items-center justify-center text-white"
+      >
         로테이션 정보
       </Link>
     </header>
