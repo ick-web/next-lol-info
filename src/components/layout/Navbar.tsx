fd93aee6464868e06a03133f4b1941bead430fdb
ick-web <yih000098@gmail.com>
2025-03-14 16:38:34 +0900

    design: 네비바 css 수정

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,13 +3,21 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <header className="flex flex-row justify-around border p-4">
+    <header className="flex flex-row justify-around p-4 bg-gray-800">
       <div>
-        <Link href="/">홈</Link>
+        <Link href="/" className="text-white">
+          홈
+        </Link>
       </div>
-      <Link href="/champions">챔피언 목록</Link>
-      <Link href="/items">아이템 목록</Link>
-      <Link href="/rotation">로테이션 정보</Link>
+      <Link href="/champions" className="text-white">
+        챔피언 목록
+      </Link>
+      <Link href="/items" className="text-white">
+        아이템 목록
+      </Link>
+      <Link href="/rotation" className="text-white">
+        로테이션 정보
+      </Link>
     </header>
   );
 };
