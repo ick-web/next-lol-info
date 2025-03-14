17bc27aefe9c34e83a5d85c61ef2946452e2167a
ick-web <yih000098@gmail.com>
2025-03-14 16:37:04 +0900

    feat: 푸터 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar/>
+        <Navbar />
         {children}
+        <footer className="bg-gray-800 p-4 mt-8 fixed bottom-0 w-full">
+          <div className="container mx-auto text-center text-white text-sm">
+            [Your Product Name] is not endorsed by Riot Games and does not
+            reflect the views or opinions of Riot Games or anyone officially
+            involved in producing or managing Riot Games properties. Riot Games
+            and all associated properties are trademarks or registered
+            trademarks of Riot Games, Inc.
+          </div>
+        </footer>
       </body>
     </html>
   );
