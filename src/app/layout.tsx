0fc77eae1f2b2c087a11bba9276c80afc2c81698
ick-web <yih000098@gmail.com>
2025-03-19 10:54:32 +0900

    ♻️ refactor: footer 컴포넌트 분리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Providers from "./providers";
+import Footer from "@/components/layout/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -18,7 +19,6 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "League of Legend Info",
   description: "League of Legend API를 활용한 정보 앱",
-  
 };
 
 export default function RootLayout({
@@ -32,18 +32,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        <Providers>
-        {children}
-        </Providers>
-        <footer className="fixed bottom-0 mt-8 w-full bg-gray-800 p-4">
-          <div className="container mx-auto text-center text-sm text-white">
-            [Your Product Name] is not endorsed by Riot Games and does not
-            reflect the views or opinions of Riot Games or anyone officially
-            involved in producing or managing Riot Games properties. Riot Games
-            and all associated properties are trademarks or registered
-            trademarks of Riot Games, Inc.
-          </div>
-        </footer>
+        <Providers>{children}</Providers>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 mt-8 w-full bg-gray-800 p-4">
+      <div className="container mx-auto text-center text-sm text-white">
+        [Your Product Name] is not endorsed by Riot Games and does not reflect
+        the views or opinions of Riot Games or anyone officially involved in
+        producing or managing Riot Games properties. Riot Games and all
+        associated properties are trademarks or registered trademarks of Riot
+        Games, Inc.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
