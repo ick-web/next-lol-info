029a9c3e96da9f2097ac48142a934488cd0907e2
ick-web <yih000098@gmail.com>
2025-03-19 10:46:49 +0900

    ♻️ refactor: 홈 페이지 컴포넌트 분리

---
+++ src/components/ui/HomeList.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const HomeList = () => {
+  return (
+    <div className="container mx-auto mt-10">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold text-red-400">
+          리그 오브 레전드 정보 앱
+        </h1>
+        <p className="mt-4 text-gray-500">
+          Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다
+        </p>
+      </div>
+      <div className="mt-[40px] flex flex-col justify-center gap-10 lg:flex-row">
+        <Link href="champions">
+          <div className="flex flex-col items-center justify-center gap-10">
+            <Image
+              src={
+                "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Kaisa_0.jpg"
+              }
+              priority={true}
+              width={400}
+              height={300}
+              alt="챔피언 목록 보기"
+              className="flex flex-col items-center justify-center"
+            />
+            <p className="text-amber-400">챔피언 목록 보기</p>
+          </div>
+        </Link>
+        <Link href="/rotation">
+          <div className="flex flex-col items-center justify-center gap-10">
+            <Image
+              src={
+                "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Jhin_4.jpg"
+              }
+              priority={true}
+              width={400}
+              height={300}
+              alt="금주 로테이션 확인"
+              className="flex flex-col items-center justify-center"
+            />
+            <p className="text-amber-400">금주 로테이션 확인</p>
+          </div>
+        </Link>
+        <Link href="/items">
+          <div className="flex flex-col items-center justify-center gap-10">
+            <Image
+              src={
+                "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Fiddlesticks_8.jpg"
+              }
+              priority={true}
+              width={400}
+              height={300}
+              alt="아이템 목록 보기"
+              className="flex flex-col items-center justify-center"
+            />
+            <p className="text-amber-400">아이템 목록 보기</p>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default HomeList;
