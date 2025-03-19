3aeb3d329ce034d60b2070568078e1bfd7c15c13
ick-web <yih000098@gmail.com>
2025-03-19 10:05:22 +0900

    🎨 design: css 수정

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -20,12 +20,14 @@ const RotationPage = () => {
     },
   });
 
-  if (isPending) return <div>로딩 중...</div>;
+  if (isPending) return <div>Loading...</div>;
   if (isError) return <p>Error: {error.message}</p>;
 
   return (
     <>
-      <h1 className="text-2xl text-red-400 font-semibold px-10 mt-7">이번 주 무료로 플레이 가능한 챔피언들을 확인해보세요!</h1>
+      <h1 className="mt-7 px-10 text-2xl font-semibold text-red-400">
+        이번 주 무료로 플레이 가능한 챔피언들을 확인해보세요!
+      </h1>
       {freeChampions?.length > 0 && <ChampionList champions={freeChampions} />}
     </>
   );
